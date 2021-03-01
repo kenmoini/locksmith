55977e1de1ddfb6022dfee7ea64d6966a114e1d8
Ken Moini <ken@kenmoini.com>
2021-02-28 20:19:20 -0500

    fix error reporting in crl

--- func.crl.go
+++ func.crl.go
@@ -70,15 +70,15 @@ func NewCRLFromFile(serialList, issuerFile, keyFile []byte, expiryTime string) (
 // CreateCRLObject will create the CRL Object
 func CreateCRLObject(certList []pkix.RevokedCertificate, key crypto.Signer, issuingCert *x509.Certificate, expiryTime time.Time) ([]byte, error) {
 	if certList == nil {
-		return nil, newError("Missing certificate list required to create CRL.")
+		return nil, &errorString{"Missing certificate list required to create CRL."}
 	}
 	if key == nil {
-		return nil, newError("Missing issuing key required to create CRL.")
+		return nil, &errorString{"Missing issuing key required to create CRL."}
 	}
 	if issuingCert == nil {
-		return nil, newError("Missing issuing certificate required to create CRL.")
+		return nil, &errorString{"Missing issuing certificate required to create CRL."}
 	}
-	if expiryTime == nil {
+	if expiryTime.IsZero() {
 		expiryTime = time.Now().AddDate(1, 0, 0)
 	}
 	crlBytes, err := issuingCert.CreateCRL(rand.Reader, key, certList, time.Now(), expiryTime)
--- func.logging.go
+++ func.logging.go
@@ -5,12 +5,20 @@ import (
 	"net/http"
 )
 
+// logNetworkRequestStdOut adds a logger wrapper to add extra network client information to the log
 func logNeworkRequestStdOut(s string, r *http.Request) {
 	logStdOut("IP[" + ReadUserIP(r) + "] UA[" + r.UserAgent() + "] " + string(s))
 	//log.Printf("[%s] %s\n", ReadUserIP(r), string(s))
 }
+
+// logStdOut just logs something to stdout
 func logStdOut(s string) {
 	log.Printf("%s\n", string(s))
+}
+
+// logStdErr just logs to stderr
+func logStdErr(s string) {
+	log.Fatalf("%s\n", string(s))
 }
 
 // check does error checking
@@ -20,7 +28,7 @@ func check(e error) {
 	}
 }
 
-// checkAndFail
+// checkAndFail checks for an error type and fails
 func checkAndFail(e error) {
 	if e != nil {
 		log.Fatalf("error: %v", e)
