5b95ef840d442f4e466a7c52a39e6c1ddc0f303b
Ken Moini <ken@kenmoini.com>
2021-03-04 22:50:56 -0500

    idk

--- types.go
+++ types.go
@@ -4,15 +4,52 @@ import (
 	"time"
 )
 
+// errorString is a trivial implementation of error.
+type errorString struct {
+	s string
+}
+
+func (e *errorString) Error() string {
+	return e.s
+}
+
 // Config struct for webapp config
 type Config struct {
 	Locksmith LocksmithYaml `yaml:"locksmith"`
 }
 
-// LocksmithYaml is what is defined for this Pilot Light server
+// LocksmithYaml is what is defined for this Locksmith server
 type LocksmithYaml struct {
 	PKIRoot string `yaml:"pki_root"`
 	Server  Server `yaml:"server"`
+}
+
+// Server configures the HTTP server
+type Server struct {
+	// Host is the local machine IP Address to bind the HTTP Server to
+	Host string `yaml:"host"`
+
+	BasePath string `yaml:"base_path"`
+
+	// Port is the local machine TCP Port to bind the HTTP Server to
+	Port    string `yaml:"port"`
+	Timeout struct {
+		// Server is the general server timeout to use
+		// for graceful shutdowns
+		Server time.Duration `yaml:"server"`
+
+		// Write is the amount of time to wait until an HTTP server
+		// write opperation is cancelled
+		Write time.Duration `yaml:"write"`
+
+		// Read is the amount of time to wait until an HTTP server
+		// read operation is cancelled
+		Read time.Duration `yaml:"read"`
+
+		// Read is the amount of time to wait
+		// until an IDLE HTTP session is closed
+		Idle time.Duration `yaml:"idle"`
+	} `yaml:"timeout"`
 }
 
 // ReturnGenericMessage - Generic message
@@ -63,34 +100,6 @@ func (self *Counter) increment() {
 	self.count++
 }
 
-// Server configures the HTTP server providing Ignition files
-type Server struct {
-	// Host is the local machine IP Address to bind the HTTP Server to
-	Host string `yaml:"host"`
-
-	BasePath string `yaml:"base_path"`
-
-	// Port is the local machine TCP Port to bind the HTTP Server to
-	Port    string `yaml:"port"`
-	Timeout struct {
-		// Server is the general server timeout to use
-		// for graceful shutdowns
-		Server time.Duration `yaml:"server"`
-
-		// Write is the amount of time to wait until an HTTP server
-		// write opperation is cancelled
-		Write time.Duration `yaml:"write"`
-
-		// Read is the amount of time to wait until an HTTP server
-		// read operation is cancelled
-		Read time.Duration `yaml:"read"`
-
-		// Read is the amount of time to wait
-		// until an IDLE HTTP session is closed
-		Idle time.Duration `yaml:"idle"`
-	} `yaml:"timeout"`
-}
-
 // CertificateConfiguration is a struct to pass Certificate Config Information into the setup functions
 type CertificateConfiguration struct {
 	Subject                 CertificateConfigurationSubject `json:"subject"`
@@ -109,15 +118,6 @@ type CertificateConfigurationSubject struct {
 	Locality           []string `json:"locality,omitempty"`
 	StreetAddress      []string `json:"street_address,omitempty"`
 	PostalCode         []string `json:"postal_code,omitempty"`
-}
-
-// errorString is a trivial implementation of error.
-type errorString struct {
-	s string
-}
-
-func (e *errorString) Error() string {
-	return e.s
 }
 
 type basicConstraints struct {
