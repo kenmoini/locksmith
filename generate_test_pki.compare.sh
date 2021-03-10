b64f72fda6af319bca191b24b2f1c236091eb6c3
Ken Moini <ken@kenmoini.com>
2021-03-10 12:52:29 -0500

    improve capability comparison

--- generate_test_pki.compare.sh
+++ generate_test_pki.compare.sh
@@ -3,6 +3,10 @@
 # Exits on any error
 set -e
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 ####################################################################
 ## Generate PKI for Tests
 ####################################################################
@@ -10,6 +14,82 @@ set -e
 ##  You can use this to instantiate your own basic PKI quickly,
 ##  however this is used primarily to test the Golang generated
 ##  PKI against a known working PKI generated via OpenSSL
+
+function splitLinesFront {  
+  echo "$1" | while IFS= read -r line ; do
+    if [[ $line != *"Certificate purposes:"* ]]; then
+      #echo $line
+      readarray -d : -t strarr <<< "$line"
+      echo "${strarr[0]}"
+    fi
+  done
+}
+function splitLinesBack {  
+  echo "$1" | while IFS= read -r line ; do
+    if [[ $line != *"Certificate purposes:"* ]]; then
+      #echo $line
+      readarray -d : -t strarr <<< "$line"
+      echo "${strarr[1]}"
+    fi
+  done
+}
+
+function splitPurposes {
+  DEF_LINES=()
+  DEF_ARRVAR=()
+  OSSL_LINES=()
+  OSSL_ARRVAR=()
+  LOCK_LINES=()
+  LOCK_ARRVAR=()
+
+  OSSL_PURPOSE="$1"
+  LOCK_PURPOSE="$2"
+  
+  DEF_LINES=$(splitLinesFront "$OSSL_PURPOSE")
+  OSSL_LINES=$(splitLinesBack "$OSSL_PURPOSE")
+  LOCK_LINES=$(splitLinesBack "$LOCK_PURPOSE")
+
+  echo "$DEF_LINES" | {
+    while IFS= read -r ROW; do
+      if [[ $ROW != "" ]]; then
+        DEF_ARRVAR+=("$ROW")
+      fi
+    done
+    echo "$OSSL_LINES" | {
+      while IFS= read -r OROW; do
+        if [[ $OROW != "" ]]; then
+          OSSL_ARRVAR+=("$OROW")
+        fi
+      done
+      
+      echo "$LOCK_LINES" | {
+        while IFS= read -r LROW; do
+          if [[ $LROW != "" ]]; then
+            LOCK_ARRVAR+=("$LROW")
+          fi
+        done
+        
+        COUNTER_INDX=0
+        echo -e "------------------------------------------OSSL---LOCK-"
+        for i in "${DEF_ARRVAR[@]}"
+        do
+          line='                                                  '
+          setOSSL_D="${OSSL_ARRVAR[$COUNTER_INDX]}"
+          COLOR=$RED
+          if [[ "$setOSSL_D" == "${LOCK_ARRVAR[$COUNTER_INDX]}" ]]; then
+            COLOR=$GREEN
+          fi
+          printf "${COLOR}%-40s %-6s %-6s${NC}" "$i" "$setOSSL_D" "${LOCK_ARRVAR[$COUNTER_INDX]}"
+          echo -e "\n------------------------------------------------------"
+          #printf "%s %s %s\n" "${line:${#i}}" "$setOSSL_D" "${LOCK_ARRVAR[$COUNTER_INDX]}"
+          #echo -e "$i:${setOSSL_D} :${LOCK_ARRVAR[$COUNTER_INDX]}"
+          let COUNTER_INDX=COUNTER_INDX+1
+        done
+      }
+    }
+  }
+
+}
 
 function checkForProgramAndExit() {
     command -v $1
@@ -46,3 +126,17 @@ echo "Lock: $(openssl x509 -in ${LOCKSMITH_PKI_ROOT_DIR}/certs/ca.pem -noout -st
 echo -e "\n===== ENDDATE COMPARISON\n"
 echo "OSSL: $(openssl x509 -in ${OPENSSL_PKI_ROOT_DIR}/ca.cert.pem -noout -enddate)"
 echo "Lock: $(openssl x509 -in ${LOCKSMITH_PKI_ROOT_DIR}/certs/ca.pem -noout -enddate)"
+
+echo -e "\n===== SERIAL COMPARISON\n"
+echo "OSSL: $(openssl x509 -in ${OPENSSL_PKI_ROOT_DIR}/ca.cert.pem -noout -serial)"
+echo "Lock: $(openssl x509 -in ${LOCKSMITH_PKI_ROOT_DIR}/certs/ca.pem -noout -serial)"
+
+echo -e "\n===== EMAIL COMPARISON\n"
+echo "OSSL: $(openssl x509 -in ${OPENSSL_PKI_ROOT_DIR}/ca.cert.pem -noout -email)"
+echo "Lock: $(openssl x509 -in ${LOCKSMITH_PKI_ROOT_DIR}/certs/ca.pem -noout -email)"
+
+echo -e "\n===== PURPOSE COMPARISON\n"
+OSSP_PURPOSE_CMD=$(openssl x509 -in ${OPENSSL_PKI_ROOT_DIR}/ca.cert.pem -noout -purpose)
+LOCK_PURPOSE_CMD=$(openssl x509 -in ${LOCKSMITH_PKI_ROOT_DIR}/certs/ca.pem -noout -purpose)
+
+splitPurposes "$OSSP_PURPOSE_CMD" "$LOCK_PURPOSE_CMD"
